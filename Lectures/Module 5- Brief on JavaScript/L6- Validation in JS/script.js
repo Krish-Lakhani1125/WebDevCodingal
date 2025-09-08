180bc9d92b87cdfee87ad76afb1d0adfc2b96ad6
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2025-09-08 18:05:56 -0500

    Add files via upload

---
+++ Lectures/Module 5- Brief on JavaScript/L6- Validation in JS/script.js
@@ -0,0 +1,30 @@
+//clears the calculator display
+function clearScreen() {
+    document.getElementById("result").value="";
+}
+//appends the clicked button's value to the display
+function setScreenValue(value) {
+document.getElementById("result"). value += value;
+
+}
+
+//calculates and displays the result
+function calculateResult() {
+    const resultElement = document.getElementById("result");
+    const expression = resultElement.value.trim();
+
+    //check for empty input
+    if (expression === '') {
+        resultElement.value = 'enter an expression';
+        return;
+    }
+
+    //evaluate expression and handle errors
+    try {
+        resultElement.value = eval(expression);
+
+    } catch (e) {
+        resultElement.value = 'Invalid expression';
+    }
+}
+
