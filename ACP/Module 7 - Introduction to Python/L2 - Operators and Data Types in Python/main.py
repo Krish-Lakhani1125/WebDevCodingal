12ce8754189eab8fae30a9e0d5936242e94dfbc1
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2025-10-17 20:03:24 -0500

    Add files via upload

---
+++ ACP/Module 7 - Introduction to Python/L2 - Operators and Data Types in Python/main.py
@@ -0,0 +1,13 @@
+name = "Krish"
+print("First Name :", name)
+print("string Sliced :", name[1:3])
+print("Length of String :", len(name))
+num1 = 25
+num2 = 16
+
+print(num1)
+print(num2)
+print(num1+num2)
+print("Number 2 Greater?", num1 < num2)
+print("Not Equal ?", num1!=num2)
+
