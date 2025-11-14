23bee772cc98c5fcf390a8215fa15ea7e28fa41f
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2025-11-13 18:07:19 -0600

    Add files via upload

---
+++ ACP/Module 7 - Introduction to Python/L5- Function/main.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+num1 = int(input ("give first number:"))
+num2 = int(input("give the second number:"))
+
+print("add:", add(num1,num2))
+print("subtract:", subtract(num1,num2))
+print("multiply:", multiply(num1,num2))
+print("divide:", divide(num1,num2))
