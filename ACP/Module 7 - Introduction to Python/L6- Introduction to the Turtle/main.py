95fb4b62a453e3d5d9c9a2b42d46989b239a7294
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2025-11-17 17:49:16 -0600

    Add files via upload

---
+++ ACP/Module 7 - Introduction to Python/L6- Introduction to the Turtle/main.py
@@ -0,0 +1,34 @@
+import turtle
+turtle = turtle.Turtle()
+turtle.forward(100)  
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.penup()
+turtle.forward(200)
+turtle.pendown()
+
+turtle.forward(100)  
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.penup()
+turtle.left(2)
+turtle.forward(200)
+turtle.pendown()
+
+turtle.forward(100)  
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
