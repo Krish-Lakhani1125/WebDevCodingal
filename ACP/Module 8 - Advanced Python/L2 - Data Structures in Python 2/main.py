18c8401a03385629fef6003364bd53672268ae4c
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2025-11-28 16:38:14 -0600

    Add files via upload

---
+++ ACP/Module 8 - Advanced Python/L2 - Data Structures in Python 2/main.py
@@ -0,0 +1,7 @@
+personal_details_tuple = ("John", "Doe", 30, 175, 70, "Mathematics")
+
+personal_details_list = list(personal_details_tuple)
+
+# Print the original tuple and the converted list
+print("Original Tuple:", personal_details_tuple)
+print("Converted List:", personal_details_list)
