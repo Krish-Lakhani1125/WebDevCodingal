d09782bd457d2aa77fae715bb02cc913b5905cb4
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2026-01-12 17:34:47 -0600

    Add files via upload

---
+++ Lectures/Module_9-Specialization_in_Python/L1-File_Handling/main.py
@@ -0,0 +1,13 @@
+file_path = r"C:/Users/Krina V/Desktop/Codingal/Web_Dev_Codingal/Lectures/Module_9-Specialization_in_Python/L1-File_Handling/Codingal.txt"
+
+with open(file_path, "r") as f:
+
+    print(f.read())
+# open file and sotre file object in a variable
+#file = open('Codingal.txt')
+
+#read the contents of file
+#print(file.read())
+
+#close the file
+#file.close()
