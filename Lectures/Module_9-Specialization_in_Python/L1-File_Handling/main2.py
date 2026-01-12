ecca2c8eaddd32be8835dbb4c99b7cd55bb419ec
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2026-01-12 17:44:04 -0600

    Add files via upload

---
+++ Lectures/Module_9-Specialization_in_Python/L1-File_Handling/main2.py
@@ -0,0 +1,16 @@
+file_path = r"C:/Users/Krina V/Desktop/Codingal/Web_Dev_Codingal/Lectures/Module_9-Specialization_in_Python/L1-File_Handling/Codingal.txt"
+with open(file_path, "r") as f:
+    print("File in Read Mode -")
+    print(f.read())
+    f.close()
+with open(file_path, "w") as f:
+    #write in the file
+    f.write(" File in write mode ....")
+    f.write("Hi! I am prenguin. I am 1 yr. old")
+    f.close()
+with open(file_path, "a") as f:
+    #open the file in append mode
+    #append in the file
+    f.write("/n File in append mode....")
+    f.write("Hi! I am Penguin. I am 1 yr. old")
+    f.close()
