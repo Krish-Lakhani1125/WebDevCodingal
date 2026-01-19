38caadb09dafcc9d98b4c5b617ef4b89689ff660
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2026-01-19 17:26:38 -0600

    Add files via upload

---
+++ Lectures/Module_9-Specialization_in_Python/L2-Operations_on_a_File_part_1/main3.py
@@ -0,0 +1,21 @@
+# Program to remove lines starting with any prefix
+file1 = open('Codingal.txt','r')
+file2 = open('CodingalUpdated.txt','w')
+
+#reading each line from original
+#text file
+for line in file1.readlines():
+    #reading all lines that do not
+    #being with "Coding"
+    if not (line.startswith('Coding')):
+
+        #print those lines
+        print(line)
+
+        #storing only those lines that
+        #do not being with "Coding"
+        file2.write(line)
+
+#close and save the files
+file2.close()
+file1.close()
