e4115386d2eb865c25fc4cb320825b3961887b94
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2026-05-01 15:32:24 -0500

    Add files via upload

---
+++ ACP/SQL ACP/employee3.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employee (
+    NAME TEXT,
+    STATUS TEXT
+);
+
+INSERT INTO employee (NAME, STATUS) VALUES
+('Smith', 'BAD'),
+('Jores','BAD'),
+('Blake', 'GOOD'),
+('Clarke', 'GOOD'),
+('Adams', 'BAD');
+
+SELECT * FROM employee;
