bfd60bbdc354cd531f1351c898c160c5de1124b1
Krish-Lakhani1125 <krishlakhani2020@gmail.com>
2026-05-29 18:16:15 -0500

    Add files via upload

---
+++ Lectures/Module 13-SQL using Python II/L1- Welcome to SQLite using Python/guessthetable.py
@@ -0,0 +1,8 @@
+print("Is the following table relational or non-relational ")
+answer = int(input("Enter your guess:"))
+if answer == 2:
+    print("Your guess was right")
+else:
+    print("Your guess was wrong")
+
+print("\n Why did you pick this answer?")
